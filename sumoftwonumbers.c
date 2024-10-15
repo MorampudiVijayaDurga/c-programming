43afd152f968e2d6525dc5ddc15b04863d020453
MorampudiVijayaDurga <vijayavijayadurga94@gmail.com>
2024-10-15 19:53:20 +0530

    Add files via upload

---
+++ sumoftwonumbers.c
@@ -0,0 +1,12 @@
+//sum of two numbers
+#include <stdio.h>
+int sum,n,m;
+int main()
+{
+  printf("please enter first numbert:",n);
+  scanf("%d",&n);
+  printf("enter second number:",m);
+  scanf("%d",&m);
+  sum=m+n;
+  printf("sum of %d and %dis %d ",n,m,sum);
+}
