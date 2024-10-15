74f1d19d71ebaa3b0288ea469d13bab17743cedc
MorampudiVijayaDurga <vijayavijayadurga94@gmail.com>
2024-10-15 19:54:20 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,18 @@
+//even or odd
+#include <stdio.h>
+int n;
+int main()
+{
+  printf("please enter a number:",n);
+  scanf("%d",&n);
+  if (n%2==0)
+  {
+      printf("Given number is even");
+  
+  }
+  else
+  {
+      printf("Given num is odd ");
+  }
+  
+}
